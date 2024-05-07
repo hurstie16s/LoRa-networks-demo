cbc569ffecae563294c365122936bc5f4cfb065a
hurstie16s <samuel.hurst2@btinternet.com>
2024-05-07 12:22:22 +0100

    corrections

--- main.py
+++ main.py
@@ -39,10 +39,11 @@ tty.setcbreak(sys.stdin.fileno())
 
 #    The following is to obtain the temprature of the RPi CPU 
 def get_cpu_temp():
-    tempFile = open( "/sys/class/thermal/thermal_zone0/temp" )
+    tempFile = open("/sys/class/thermal/thermal_zone0/temp")
     cpu_temp = tempFile.read()
     tempFile.close()
-    return float(cpu_temp)/1000
+    return float(cpu_temp) / 1000
+
 
 #   serial_num
 #       PiZero, Pi3B+, and Pi4B use "/dev/ttyS0"
@@ -63,17 +64,20 @@ def get_cpu_temp():
 #
 
 # node = sx126x.sx126x(serial_num = "/dev/ttyS0",freq=433,addr=0,power=22,rssi=False,air_speed=2400,relay=False)
-node = sx126x.sx126x(serial_num = "/dev/ttyS0",freq=868,addr=0,power=22,rssi=True,air_speed=2400,relay=False)
+node = sx126x.sx126x(serial_num="/dev/ttyS0", freq=868, addr=0, power=22, rssi=True, air_speed=2400, relay=False)
+
 
 def send_deal():
     get_rec = ""
     print("")
-    print("input a string such as \033[1;32m0,868,Hello World\033[0m,it will send `Hello World` to lora node device of address 0 with 868M ")
-    print("please input and press Enter key:",end='',flush=True)
+    print(
+        "input a string such as \033[1;32m0,868,Hello World\033[0m,it will send `Hello World` to lora node device of "
+        "address 0 with 868M ")
+    print("please input and press Enter key:", end='', flush=True)
 
     while True:
         rec = sys.stdin.read(1)
-        if rec != None:
+        if rec is not None:
             if rec == '\x0a': break
             get_rec += rec
             sys.stdout.write(rec)
@@ -81,49 +85,54 @@ def send_deal():
 
     get_t = get_rec.split(",")
 
-    offset_frequence = int(get_t[1])-(850 if int(get_t[1])>850 else 410)
+    offset_frequence = int(get_t[1]) - (850 if int(get_t[1]) > 850 else 410)
     #
     # the sending message format
     #
     #         receiving node              receiving node                   receiving node           own high 8bit           own low 8bit                 own 
     #         high 8bit address           low 8bit address                    frequency                address                 address                  frequency             message payload
-    data = bytes([int(get_t[0])>>8]) + bytes([int(get_t[0])&0xff]) + bytes([offset_frequence]) + bytes([node.addr>>8]) + bytes([node.addr&0xff]) + bytes([node.offset_freq]) + get_t[2].encode()
+    data = bytes([int(get_t[0]) >> 8]) + bytes([int(get_t[0]) & 0xff]) + bytes([offset_frequence]) + bytes(
+        [node.addr >> 8]) + bytes([node.addr & 0xff]) + bytes([node.offset_freq]) + get_t[2].encode()
 
     node.send(data)
-    print('\x1b[2A',end='\r')
-    print(" "*200)
-    print(" "*200)
-    print(" "*200)
-    print('\x1b[3A',end='\r')
+    print('\x1b[2A', end='\r')
+    print(" " * 200)
+    print(" " * 200)
+    print(" " * 200)
+    print('\x1b[3A', end='\r')
 
-def send_cpu_continue(continue_or_not = True):
+
+def send_cpu_continue(continue_or_not=True):
     if continue_or_not:
         global timer_task
         global seconds
         #
         # boarcast the cpu temperature at 868.125MHz
         #
-        data = bytes([255]) + bytes([255]) + bytes([18]) + bytes([255]) + bytes([255]) + bytes([12]) + "CPU Temperature:".encode()+str(get_cpu_temp()).encode()+" C".encode()
+        data = bytes([255]) + bytes([255]) + bytes([18]) + bytes([255]) + bytes([255]) + bytes(
+            [12]) + "CPU Temperature:".encode() + str(get_cpu_temp()).encode() + " C".encode()
         node.send(data)
         time.sleep(0.2)
-        timer_task = Timer(seconds,send_cpu_continue)
+        timer_task = Timer(seconds, send_cpu_continue)
         timer_task.start()
     else:
-        data = bytes([255]) + bytes([255]) + bytes([18]) + bytes([255]) + bytes([255]) + bytes([12]) + "CPU Temperature:".encode()+str(get_cpu_temp()).encode()+" C".encode()
+        data = bytes([255]) + bytes([255]) + bytes([18]) + bytes([255]) + bytes([255]) + bytes(
+            [12]) + "CPU Temperature:".encode() + str(get_cpu_temp()).encode() + " C".encode()
         node.send(data)
         time.sleep(0.2)
         timer_task.cancel()
         pass
+
 
 try:
     time.sleep(1)
     print("Press \033[1;32mEsc\033[0m to exit")
     print("Press \033[1;32mi\033[0m   to send")
     print("Press \033[1;32ms\033[0m   to send cpu temperature every 10 seconds")
-    
+
     # it will send rpi cpu temperature every 10 seconds 
     seconds = 10
-    
+
     while True:
 
         if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
@@ -137,23 +146,23 @@ try:
             # dectect key s
             if c == '\x73':
                 print("Press \033[1;32mc\033[0m   to exit the send task")
-                timer_task = Timer(seconds,send_cpu_continue)
+                timer_task = Timer(seconds, send_cpu_continue)
                 timer_task.start()
-                
+
                 while True:
                     if sys.stdin.read(1) == '\x63':
                         timer_task.cancel()
-                        print('\x1b[1A',end='\r')
-                        print(" "*100)
-                        print('\x1b[1A',end='\r')
+                        print('\x1b[1A', end='\r')
+                        print(" " * 100)
+                        print('\x1b[1A', end='\r')
                         break
 
             sys.stdout.flush()
-            
+
         node.receive()
-        
+
         # timer,send messages automatically
-        
+
 except:
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
     # print('\x1b[2A',end='\r')
