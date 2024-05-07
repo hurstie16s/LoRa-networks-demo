645cc79afb30864107e9e2573345a529b93e14f8
hurstie16s <samuel.hurst2@btinternet.com>
2024-05-07 15:44:12 +0100

    send ack for join

--- pi0.py
+++ pi0.py
@@ -104,7 +104,7 @@ def main():
 
         address, message, flag = node.receive_gateway()
         if flag:
-            message = message.decode()
+            message = message.decode('utf-8')
             print(message)
             if "ACK-JOIN:" in message:
                 # Acknowledge join, change address
